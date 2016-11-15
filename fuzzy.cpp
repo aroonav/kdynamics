e7dd52efe1346347e2819f3aa592d10ac7dcb350
Aroonav Mishra <aroonav11@gmail.com>
2016-11-15 23:33:15 +0530

    Moved code into a function. Code is now cleaner.

--- fuzzy.cpp
+++ fuzzy.cpp
@@ -18,29 +18,23 @@ float membership_value(float input, float lowValue, float midValue, float highVa
 		return (highValue-input)/(highValue-midValue);
 }
 
-// Fuzzy Inference System learning component
-void fis_learning()
+// This returns the membership for the delays in an array ``vector''. It can range from 0-4, i.e from Very Fast to Very Slow
+int gruntWork(int* vector, int noOfTries, int noOfFuzzySets)
 {
-	// TODO: Open the ruleBase.txt file and output the results of a particular user into that file.
-	const int usernameLength = 4;		// Length of the username. ex "s001"
-	const int passwordLength = 11;		// Length of the password ".tie5Roanl" + Return Key.
-	const int noOfTries = 15;			// No. of tries done to train/create fuzzy rule for a single user
-	const int noOfUsers = 50;			// No. of users for which this model is being built
-	const int noOfFuzzySets = 5;		// No. of fuzzy sets, here 5 i.e Very Fast(0), Fast(1), Moderate(2), Slow(3), Very Slow(4)
-	int finalMembership = -1;									// This denotes the final membership of the delays given in the vector. That is, whether the delays given in the vector is Very Fast, Fast, Moderate, Slow, Very Slow.
-	float membership_values[noOfTries][noOfFuzzySets];memset(membership_values, 0, sizeof(membership_values));	// This contains the membership values where each value denotes the membership value of that particular row in the column's fuzzy set.
-	float weightedSum = 0.0;float membershipSum = 0.0;			// weightedSum and membershipSum is the numerator and denominator respetively for the calculation of the weightedAverage using COG method during defuzzification.
+	int finalMembership = -1;
+	float membership_values[noOfTries][noOfFuzzySets];			// This contains the membership values where each value denotes the membership value of that particular row in the column's fuzzy set.
 	float weightedAverage_memberships[noOfFuzzySets];			// After the defuzzification, the value is again fuzzified and this array contains the membership values for the defuzzified value in the $noOfFuzzySets fuzzy sets
+	float max = 0.0;
+	float weightedSum = 0.0;float membershipSum = 0.0;			// weightedSum and membershipSum is the numerator and denominator respetively for the calculation of the weightedAverage using COG method during defuzzification.
 	float classifiers[5][3]={	//Low value, Middle Value, High Value
-								{21,		 25.5,			29},	// Very Fast(0)
-								{26, 		 29,			32},	// Fast		(1)
-								{30, 		 33.5,			37},	// Moderate	(2)
-								{36, 		 40,			44},	// Slow		(3)
-								{42,		 46,			50}		// Very Slow(4)
-							};
+							{21,		 25.5,			29},	// Very Fast(0)
+							{26, 		 29,			32},	// Fast		(1)
+							{30, 		 33.5,			37},	// Moderate	(2)
+							{36, 		 40,			44},	// Slow		(3)
+							{42,		 46,			50}		// Very Slow(4)
+						};
+	memset(membership_values, 0, sizeof(membership_values));
 
-
-	int vector[noOfTries] = {27, 33, 24, 49, 27, 33, 33, 33, 33, 33, 33, 28, 33, 28, 27};						/* TODO: This vector has been defined temporarily. Later, take the values from the file for that particular username and then proceed to form the rule base. Each value of the noOfTries values in the vector is the delay(in *10^(-2) seconds) between two characters in the password. */
 	for (int i = 0; i < noOfTries; i++)							// This finds the union of the fuzzy sets by finding the maximum of all the membership values for a particular row. It then uses this value to find the numerator and denominator to find the weightedAverage.
 	{
 		float max = 0.0;
@@ -53,8 +47,6 @@ void fis_learning()
 		weightedSum += (max*vector[i]);							// Numerator for weighted average
 		membershipSum += max;									// Denominator for weighted average
 	}
-
-	float max = 0.0;
 	float weightedAverage_cog = weightedSum/membershipSum;		// Defuzzified value using Centre of Gravity method.
 	for (int i = 0; i < noOfFuzzySets; i++)						// Defuzzified value is again fuzzified in this step.
 	{
@@ -65,15 +57,27 @@ void fis_learning()
 			max = weightedAverage_memberships[i];
 		}
 	}
+	return finalMembership;
+}
+
+// Fuzzy Inference System learning component
+void fis_learning()
+{
+	const int usernameLength = 4;		// Length of the username. ex "s001"
+	const int passwordLength = 11;		// Length of the password ".tie5Roanl" + Return Key.
+	const int noOfTries = 15;			// No. of tries done to train/create fuzzy rule for a single user
+	const int noOfUsers = 50;			// No. of users for which this model is being built
+	const int noOfFuzzySets = 5;		// No. of fuzzy sets, here 5 i.e Very Fast(0), Fast(1), Moderate(2), Slow(3), Very Slow(4)
+	int finalMembership = -1;			// This denotes the final membership of the delays given in the vector. That is, whether the delays given in the vector is Very Fast, Fast, Moderate, Slow, Very Slow.
+
+	int vector[noOfTries] = {27, 33, 24, 49, 27, 33, 33, 33, 33, 33, 33, 28, 33, 28, 27};
+	finalMembership = gruntWork(vector, noOfTries, noOfFuzzySets);
+	cout<<finalMembership<<endl;
 }
 
 // Fuzzy Inference System working/testing component
 void fis_working()
 {
-	// string userName;
-	// cout<<"Enter username:";
-	// scanf("%*c");getline(cin, userName);
-	// cout<<userName<<endl;
 }
 
 // Fuzzy inference system. option denotes learning or working option.
