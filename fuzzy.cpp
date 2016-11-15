5573b12dbb5a4be58dc8c5ef503f323f566ab7cd
Aroonav Mishra <aroonav11@gmail.com>
2016-11-16 03:05:39 +0530

    Added some constants.

--- fuzzy.cpp
+++ fuzzy.cpp
@@ -6,6 +6,12 @@
 
 #define PROFILEPATH "data/profiles.txt"
 #define DATASETPATH "data/DSL-StrongPasswordData.csv"
+#define BUFFER_SIZE 512
+#define USERNAME_LENGTH 4					// Length of the username. ex "s001"
+#define PASSWORD_LENGTH 11					// Length of the password ".tie5Roanl" + Return Key.
+#define NO_OF_FUZZY_SETS 5					// No. of fuzzy sets, here 5 i.e Very Fast(0), Fast(1), Moderate(2), Slow(3), Very Slow(4)
+#define NO_OF_USERS 50						// No. of users for this FIS.
+#define NO_OF_TRIES 15						// No. of trials done to train/create fuzzy rule for a single user
 
 using namespace std;
 
@@ -23,8 +29,9 @@ float membership_value(float input, float lowValue, float midValue, float highVa
 		return (highValue-input)/(highValue-midValue);
 }
 
-int writeProfileToFile(char* username, int* profile, int noOfDataItems)
+int writeProfileToFile(char* username, int* profile)
 {
+	int noOfDataItems = PASSWORD_LENGTH - 1;			// To write to file.
 	ofstream fout;fout.open(PROFILEPATH, std::ios_base::app);
 	fout<<username<<",";
 	for (int i = 0; i < noOfDataItems; i++)
@@ -35,11 +42,11 @@ int writeProfileToFile(char* username, int* profile, int noOfDataItems)
 }
 
 // This returns the membership for the delays in an array ``vector''. It can range from 0-4, i.e from Very Fast to Very Slow
-int gruntWork(float* vector, int noOfTries, int noOfFuzzySets)
+int gruntWork(float* vector)
 {
 	int finalMembership = -1;									// Unclassified. The vector's data do not fit in any of the fuzzy sets.
-	float membership_values[noOfTries][noOfFuzzySets];			// This contains the membership values where each value denotes the membership value of that particular row in the column's fuzzy set.
-	float weightedAverage_memberships[noOfFuzzySets];			// After the defuzzification, the value is again fuzzified and this array contains the membership values for the defuzzified value in the $noOfFuzzySets fuzzy sets
+	float membership_values[NO_OF_TRIES][NO_OF_FUZZY_SETS];			// This contains the membership values where each value denotes the membership value of that particular row in the column's fuzzy set.
+	float weightedAverage_memberships[NO_OF_FUZZY_SETS];			// After the defuzzification, the value is again fuzzified and this array contains the membership values for the defuzzified value in the $NO_OF_FUZZY_SETS fuzzy sets
 	float max = 0.0;
 	float weightedSum = 0.0;float membershipSum = 0.0;			// weightedSum and membershipSum is the numerator and denominator respetively for the calculation of the weightedAverage using COG method during defuzzification.
 	float weightedAverage_cog = 0.0;
@@ -53,10 +60,10 @@ int gruntWork(float* vector, int noOfTries, int noOfFuzzySets)
 	memset(membership_values, 0, sizeof(membership_values));
 	memset(weightedAverage_memberships, 0, sizeof(weightedAverage_memberships));
 
-	for (int i = 0; i < noOfTries; i++)							// This finds the union of the fuzzy sets by finding the maximum of all the membership values for a particular row. It then uses this value to find the numerator and denominator to find the weightedAverage.
+	for (int i = 0; i < NO_OF_TRIES; i++)							// This finds the union of the fuzzy sets by finding the maximum of all the membership values for a particular row. It then uses this value to find the numerator and denominator to find the weightedAverage.
 	{
 		float max = 0.0;
-		for (int j = 0; j < noOfFuzzySets; j++)
+		for (int j = 0; j < NO_OF_FUZZY_SETS; j++)
 		{
 			membership_values[i][j] = membership_value(vector[i], classifiers[j][0], classifiers[j][1], classifiers[j][2]);
 			if (membership_values[i][j]>max)
@@ -67,7 +74,7 @@ int gruntWork(float* vector, int noOfTries, int noOfFuzzySets)
 	}
 	if(membershipSum!=0)
 		weightedAverage_cog = weightedSum/membershipSum;		// Defuzzified value using Centre of Gravity method.
-	for (int i = 0; i < noOfFuzzySets; i++)						// Defuzzified value is again fuzzified in this step.
+	for (int i = 0; i < NO_OF_FUZZY_SETS; i++)						// Defuzzified value is again fuzzified in this step.
 	{
 		weightedAverage_memberships[i] = membership_value(weightedAverage_cog, classifiers[i][0], classifiers[i][1], classifiers[i][2]);
 		if(weightedAverage_memberships[i]>max)
@@ -79,28 +86,29 @@ int gruntWork(float* vector, int noOfTries, int noOfFuzzySets)
 	return finalMembership;
 }
 
-// Fuzzy Inference System learning component
-void fis_learning(const int noOfUsers, const int noOfTries)
+void sss()
 {
-	const int BUFFER_SIZE = 512;
-	const int USERNAME_LENGTH = 4;					// Length of the username. ex "s001"
-	const int passwordLength = 11;					// Length of the password ".tie5Roanl" + Return Key.
-	const int noOfFuzzySets = 5;					// No. of fuzzy sets, here 5 i.e Very Fast(0), Fast(1), Moderate(2), Slow(3), Very Slow(4)
+
+}
+
+// Fuzzy Inference System learning component
+void fis_learning()
+{
 	int finalMembership = -1;						// This denotes the final membership of the delays given in the vector. That is, whether the delays given in the vector is Very Fast, Fast, Moderate, Slow, Very Slow.
-	char* fbuff = new char[BUFFER_SIZE];			// File Buffer
 	char* username = new char[USERNAME_LENGTH+1];
 	char* value = new char[6];
-	float delays[passwordLength-1][noOfTries];memset(delays, 0, sizeof(delays));
-	int profile[passwordLength-1];memset(profile, 0, sizeof(profile));
+	int profile[PASSWORD_LENGTH-1];memset(profile, 0, sizeof(profile));
+	float delays[PASSWORD_LENGTH-1][NO_OF_TRIES];memset(delays, 0, sizeof(delays));
+	char* fbuff = new char[BUFFER_SIZE];			// File Buffer
 
 	ifstream fin;fin.open(DATASETPATH, ios::in);
 	fin.getline(fbuff, BUFFER_SIZE);				// Removes the first line from the file
 
-	// for(int i = 0; i<noOfUsers; i++)						// For each user, create the rules
+	// for(int i = 0; i<NO_OF_USERS; i++)						// For each user, create the rules
 	for(int i = 0; i<10; i++)						// For each user, create the rules
 	{
 		memset(delays, 0, sizeof(delays));
-		for (int j = 0; j < noOfTries; j++)			// By each user's, for each attempt out of his/her 15(``noOfTries'') attempts at the password, fill up a column in a plane
+		for (int j = 0; j < NO_OF_TRIES; j++)			// By each user's, for each attempt out of his/her 15(``NO_OF_TRIES'') attempts at the password, fill up a column in a plane
 		{
 			fin.getline(fbuff, BUFFER_SIZE);						// Read one entire line of CSV into fbuff
 			string fbuffString(fbuff);								// Create a std::string fbuffString from char* fbuff
@@ -124,15 +132,15 @@ void fis_learning(const int noOfUsers, const int noOfTries)
 				}
 			}
 		}
-		for (int j = noOfTries; j < 400; j++)						// This will discard the rest of the 385 lines of CSV and move the file pointer to the next user.
+		for (int j = NO_OF_TRIES; j < 400; j++)						// This will discard the rest of the 385 lines of CSV and move the file pointer to the next user.
 			fin.getline(fbuff, BUFFER_SIZE);						// Read one entire line of CSV into fbuff
 
 		for (int j = 0; j < 10; j++)
 		{
-			finalMembership = gruntWork(delays[j], noOfTries, noOfFuzzySets);
+			finalMembership = gruntWork(delays[j]);
 			profile[j] = finalMembership;
 		}
-		if(writeProfileToFile(username, profile, passwordLength-1)==0)
+		if(writeProfileToFile(username, profile)==0)
 			cout<<"Profile of "<<username<<" appended to "<<PROFILEPATH<<endl;
 		else
 			cout<<"Error in writing to file.";
@@ -144,19 +152,17 @@ void fis_learning(const int noOfUsers, const int noOfTries)
 }
 
 // Fuzzy Inference System working/testing component
-void fis_working(const int noOfUsers, const int noOfTries)
+void fis_working()
 {
 }
 
 // Fuzzy inference system. option denotes learning or working option.
 void fis(int option)
 {
-	const int noOfUsers = 50;						// No. of users for this FIS.
-	const int noOfTries = 15;						// No. of trials done to train/create fuzzy rule for a single user
 	if(option==1)
-		fis_learning(noOfUsers, noOfTries);
+		fis_learning();
 	else
-		fis_working(noOfUsers, noOfTries);
+		fis_working();
 }
 
 int main()
