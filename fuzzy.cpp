83da1647da0ae473a7af84207d2dfdc79b47ba92
Aroonav Mishra <aroonav11@gmail.com>
2016-11-16 15:34:24 +0530

    Made classifiers data global for easy access and change when required.

--- fuzzy.cpp
+++ fuzzy.cpp
@@ -9,13 +9,22 @@
 #define BUFFER_SIZE 512
 #define USERNAME_LENGTH 4					// Length of the username. ex "s001"
 #define PASSWORD_LENGTH 11					// Length of the password ".tie5Roanl" + Return Key.
+#define UNCLASSIFIED_VALUE -10				// Value denoting Unclassification.
 #define NO_OF_FUZZY_SETS 5					// No. of fuzzy sets, here 5 i.e Very Fast(0), Fast(1), Moderate(2), Slow(3), Very Slow(4)
 #define NO_OF_TRIES 15						// No. of trials done to train/create fuzzy rule for a single user
 
 #define NO_OF_USERS 50						// No. of users for this FIS.
-#define NO_OF_TESTING_ATTEMPTS 200			// This is the number of test attempts of one particular user against a stored profile.
+#define NO_OF_TESTING_ATTEMPTS 100			// This is the number of test attempts of one particular user against a stored profile.
 
 using namespace std;
+
+float classifiers[NO_OF_FUZZY_SETS][3]={	//Low value, Middle Value, High Value
+							{21,		 25.5,			29},	// Very Fast(0)
+							{26, 		 29,			32},	// Fast		(1)
+							{30, 		 33.5,			37},	// Moderate	(2)
+							{36, 		 40,			44},	// Slow		(3)
+							{42,		 46,			50}	// Very Slow(4)
+					};
 
 // Returns the membership value for the given input depending on the other input variables
 // TODO: Take an input as an array which consists the last three values. So that the calling function can directly pass a row of classifiers 2D array.
@@ -46,19 +55,12 @@ int writeProfileToFile(char* username, int* profile)
 // This returns the membership for the delays in an array ``vector''. It can range from 0-4, i.e from Very Fast to Very Slow
 int gruntWorkForFisLearning(float* vector)
 {
-	int finalMembership = -1;									// Unclassified. The vector's data do not fit in any of the fuzzy sets.
+	int finalMembership = UNCLASSIFIED_VALUE;						// Unclassified. The vector's data do not fit in any of the fuzzy sets.
 	float membership_values[NO_OF_TRIES][NO_OF_FUZZY_SETS];			// This contains the membership values where each value denotes the membership value of that particular row in the column's fuzzy set.
 	float weightedAverage_memberships[NO_OF_FUZZY_SETS];			// After the defuzzification, the value is again fuzzified and this array contains the membership values for the defuzzified value in the $NO_OF_FUZZY_SETS fuzzy sets
 	float max = 0.0;
 	float weightedSum = 0.0;float membershipSum = 0.0;			// weightedSum and membershipSum is the numerator and denominator respetively for the calculation of the weightedAverage using COG method during defuzzification.
 	float weightedAverage_cog = 0.0;
-	float classifiers[5][3]={	//Low value, Middle Value, High Value
-							{21,		 25.5,			29},	// Very Fast(0)
-							{26, 		 29,			32},	// Fast		(1)
-							{30, 		 33.5,			37},	// Moderate	(2)
-							{36, 		 40,			44},	// Slow		(3)
-							{42,		 46,			50}		// Very Slow(4)
-						};
 	memset(membership_values, 0, sizeof(membership_values));
 	memset(weightedAverage_memberships, 0, sizeof(weightedAverage_memberships));
 
@@ -76,7 +78,7 @@ int gruntWorkForFisLearning(float* vector)
 	}
 	if(membershipSum!=0)
 		weightedAverage_cog = weightedSum/membershipSum;		// Defuzzified value using Centre of Gravity method.
-	for (int i = 0; i < NO_OF_FUZZY_SETS; i++)						// Defuzzified value is again fuzzified in this step.
+	for (int i = 0; i < NO_OF_FUZZY_SETS; i++)					// Defuzzified value is again fuzzified in this step.
 	{
 		weightedAverage_memberships[i] = membership_value(weightedAverage_cog, classifiers[i][0], classifiers[i][1], classifiers[i][2]);
 		if(weightedAverage_memberships[i]>max)
@@ -91,7 +93,7 @@ int gruntWorkForFisLearning(float* vector)
 // Fuzzy Inference System learning component
 void fis_learning()
 {
-	int finalMembership = -1;						// This denotes the final membership of the delays given in the vector. That is, whether the delays given in the vector is Very Fast, Fast, Moderate, Slow, Very Slow.
+	int finalMembership = UNCLASSIFIED_VALUE;					// This denotes the final membership of the delays given in the vector. That is, whether the delays given in the vector is Very Fast, Fast, Moderate, Slow, Very Slow. -10 denotes unclassified.
 	char* username = new char[USERNAME_LENGTH+1];
 	char* value = new char[6];
 	int profile[PASSWORD_LENGTH-1];memset(profile, 0, sizeof(profile));
@@ -203,19 +205,12 @@ int gruntWorkForFisWorking(float* vector, int* testProfile)
 {
 	float membership_values[PASSWORD_LENGTH-1][NO_OF_FUZZY_SETS];	// This contains the membership values where each value denotes the membership value of that particular row in the column's fuzzy set.
 	float max = 0.0;
-	float classifiers[5][3]={	//Low value, Middle Value, High Value
-							{21,		 25.5,			29},	// Very Fast(0)
-							{26, 		 29,			32},	// Fast		(1)
-							{30, 		 33.5,			37},	// Moderate	(2)
-							{36, 		 40,			44},	// Slow		(3)
-							{42,		 46,			50}		// Very Slow(4)
-						};
 	memset(membership_values, 0, sizeof(membership_values));
 
 	for (int i = 0; i < PASSWORD_LENGTH-1; i++)						// This finds the union of the fuzzy sets by finding the maximum of all the membership values for a particular row. It then uses this value to find the numerator and denominator to find the weightedAverage.
 	{
 		float max = 0.0;
-		int finalMembership = -1;
+		int finalMembership = UNCLASSIFIED_VALUE;
 		for (int j = 0; j < NO_OF_FUZZY_SETS; j++)
 		{
 			membership_values[i][j] = membership_value(vector[i], classifiers[j][0], classifiers[j][1], classifiers[j][2]);
